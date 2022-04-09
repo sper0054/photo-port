6fe8c9d682909f8535d25710eaa7d4b8f18e4c6a
sper0054 <teresa.sperl@gmail.com>
2022-04-09 13:55:03 -0500

    nav edits

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,17 +3,12 @@ import React from 'react';
 
 function Nav() {
     const categories = [
-        {
-            name: "commercial",
-            description: "Photos of grocery stores, food trucks, and other commercial projects",
-        },
+        { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects" },
         { name: "portraits", description: "Portraits of people in my life" },
         { name: "food", description: "Delicious delicacies" },
-        {
-            name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" ,
-        },
+        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" }
     ];
-    
+
     function categorySelected(name) {
         console.log(`${name} clicked`)
     }
