d912f5a5e9df00c130c60b6d4cc47f05eff8fca4
sper0054 <teresa.sperl@gmail.com>
2022-04-10 12:34:11 -0500

    modal

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,19 @@
-import React from 'react'; 
-
+// __tests__/About.test.js
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-
-import '@testing-library/jest-dom/extend-expect'; 
-
+import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
 
-describe('About component renders', () => {
-    it('renders', () => {
-        render(<About />);
-    });
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  });
 
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    });
+  it('matches snapshot', () => {
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -13,3 +13,4 @@ function Gallery(props) {
   );
 }
 export default Gallery;
+
