60431a6e1097ad8a1432ff3944b8ea1ba797da3e
sper0054 <teresa.sperl@gmail.com>
2022-04-10 11:31:08 -0500

    gallery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import './App.css';
+import Gallery from './components/Gallery';
 
 function App() {
+
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
